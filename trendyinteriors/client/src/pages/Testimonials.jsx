fec823ffb403f777a0ade3c00e4d06f6721eb134
AshwathNagarajan <ashwathnagarajan14@gmail.com>
2026-06-13 02:31:25 +0530

    deployment failure bug

--- trendyinteriors/client/src/pages/Testimonials.jsx
+++ trendyinteriors/client/src/pages/Testimonials.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { publicGet } from '../utils/publicApi';
-import { FaQuoteLeft, FaStar, FaPenNib, FaUserCircle } from 'react-icons/fa';
+import { FaQuoteLeft, FaStar, FaPenNib } from 'react-icons/fa';
 import './Testimonials.css';
 
 const Testimonials = () => {
